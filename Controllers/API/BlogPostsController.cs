3d941542f34dfbf65d3850e6deb09174e274e2f3
Jamal Gibson <jamal.gibson@rogers.com>
2023-06-15 22:11:33 -0400

    some updates

--- Controllers/API/BlogPostsController.cs
+++ Controllers/API/BlogPostsController.cs
@@ -21,101 +21,6 @@ namespace CrucibleBlogMVC.Controllers.API
             _context = context;
         }
 
-        // GET: api/BlogPosts
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<BlogPost>>> GetBlogPosts()
-        {
-          if (_context.BlogPosts == null)
-          {
-              return NotFound();
-          }
-            return await _context.BlogPosts.ToListAsync();
-        }
-
-        // GET: api/BlogPosts/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<BlogPost>> GetBlogPost(int id)
-        {
-          if (_context.BlogPosts == null)
-          {
-              return NotFound();
-          }
-            var blogPost = await _context.BlogPosts.FindAsync(id);
-
-            if (blogPost == null)
-            {
-                return NotFound();
-            }
-
-            return blogPost;
-        }
-
-        // PUT: api/BlogPosts/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutBlogPost(int id, BlogPost blogPost)
-        {
-            if (id != blogPost.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(blogPost).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!BlogPostExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/BlogPosts
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<BlogPost>> PostBlogPost(BlogPost blogPost)
-        {
-          if (_context.BlogPosts == null)
-          {
-              return Problem("Entity set 'ApplicationDbContext.BlogPosts'  is null.");
-          }
-            _context.BlogPosts.Add(blogPost);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetBlogPost", new { id = blogPost.Id }, blogPost);
-        }
-
-        // DELETE: api/BlogPosts/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteBlogPost(int id)
-        {
-            if (_context.BlogPosts == null)
-            {
-                return NotFound();
-            }
-            var blogPost = await _context.BlogPosts.FindAsync(id);
-            if (blogPost == null)
-            {
-                return NotFound();
-            }
-
-            _context.BlogPosts.Remove(blogPost);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
         [HttpGet]
         [Route("portfolio/{count}")]
         public async Task<ActionResult<IEnumerable<BlogPost>>> GetPortfolioBlogPosts(int? count)
@@ -131,12 +36,8 @@ namespace CrucibleBlogMVC.Controllers.API
             {
                 return Ok(result);
             }
-            return BadRequest();
-        }
 
-        private bool BlogPostExists(int id)
-        {
-            return (_context.BlogPosts?.Any(e => e.Id == id)).GetValueOrDefault();
+            return BadRequest();
         }
     }
 }
